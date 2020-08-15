553b61cead5bbc41766b0cc91e908ef4afac6736
RyosukeKamimura <come1234560@gmail.com>
2020-08-15 16:46:20 +0900

    Follow Object🐍

--- HealthKit_Tutorial/ContentView.swift
+++ HealthKit_Tutorial/ContentView.swift
@@ -10,9 +10,38 @@ import SwiftUI
 import HealthKit
 
 struct ContentView: View {
+    
+    private var healthStore: HealthStore?
+    
+    
+    init() {
+        healthStore = HealthStore()
+    }
+    
+    
+    private func updateUIFromStatistics(_ staticsCollection: HKStatisticsCollection){
+        
+    }
+    
     var body: some View {
         Text("Hello, World!").padding()
+            .onAppear{
+                if let healthStore = self.healthStore {
+                    healthStore.requestAuthorization{ success in
+                        if success {
+                            healthStore.calculateSteps { statisticsCollection in
+                                if let statisticsCollection = statisticsCollection {
+                                    //Updata UI
+                                    print(statisticsCollection)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
     }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
